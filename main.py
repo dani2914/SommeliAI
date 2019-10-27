525d46164ac86cca2670a58e240bbedc62dad25c
Ray Li <raymond8908@gmail.com>
2019-10-27 14:22:55 -0400

    Separated LDA into LDA class, main, utils. Tested for wine sub-dataset n <= 50. >100 breaks.

---
+++ main.py
@@ -0,0 +1,85 @@
+""" main driver """
+import importlib
+import util
+import lda
+import numpy as np
+import torch
+import pyro
+from pyro.optim import Adam
+from pyro.infer import TraceEnum_ELBO
+
+
+pyro.set_rng_seed(0)
+pyro.clear_param_store()
+pyro.enable_validation(True)
+
+def main():
+    """ main function """
+
+    # CONSTANTS
+    ADAM_LEARN_RATE = 0.01
+    TESTING_SUBSIZE = 50 #use None if want to use full dataset
+
+    full_df = util.fetch_dataset(n=TESTING_SUBSIZE)
+    clean_df = util.preprocess(full_df)
+
+    txt_vec = clean_df["description"]
+    topic_vec = clean_df["variety"]
+    unique_topics = np.unique(topic_vec)
+
+    indexed_txt_list, vocab_dict = util.conv_word_to_indexed_txt(txt_vec)
+
+    num_topic = len(unique_topics) 
+    num_vocab = len(vocab_dict)
+    num_txt = len(indexed_txt_list)
+    num_words_per_txt = [len(txt) for txt in indexed_txt_list]
+
+#    # purely for testing purposes, overrides original val with toy dataset
+#    indexed_txt_list = [
+#        torch.tensor([1, 2, 3, 4, 5]),
+#        torch.tensor([0, 2, 4, 6, 8, 9]),
+#        torch.tensor([1, 3, 5, 7]),
+#        torch.tensor([5, 6, 7])]
+#    num_topic = 3
+#    num_vocab = len(np.unique([word for txt in indexed_txt_list for word in txt]))
+#    num_txt = len(indexed_txt_list)
+#    num_words_per_txt = [len(txt) for txt in indexed_txt_list]
+
+    # create object of LDA class
+    orig_lda = lda.origLDA()
+
+    svi = pyro.infer.SVI(
+        model=orig_lda.model,
+        guide=orig_lda.guide,
+        optim=Adam({"lr": ADAM_LEARN_RATE}),
+        loss=TraceEnum_ELBO(max_plate_nesting=2))
+
+    losses, alpha, beta = [], [], []
+    num_step = 100
+    for step in range(num_step):
+
+        loss = svi.step(indexed_txt_list, num_txt, num_words_per_txt,
+                          num_topic, num_vocab)
+        losses.append(loss)
+        alpha.append(pyro.param("alpha_q"))
+        beta.append(pyro.param("beta_q"))
+        if step % 10 == 0:
+            print("{}: {}".format(step, np.round(loss, 1)))
+
+    # evaluate results
+    dtype = [("word", "<U17"), ("index", int)]
+    vocab = np.array([item for item in vocab_dict.items()], dtype=dtype)
+    vocab = np.sort(vocab, order="index")
+
+    posterior_doc_x_words, posterior_topics_x_words = \
+            orig_lda.model(indexed_txt_list, num_txt, num_words_per_txt,
+                           num_topic, num_vocab)
+
+    for i in range(num_topic):
+        non_trivial_words_ix = np.where(posterior_topics_x_words[i] > 0.01)[0]
+        print("topic %s" % i)
+        print([word[0] for word in vocab[non_trivial_words_ix]])
+
+if __name__ == "__main__":
+
+    main()
---
+++ util.py
@@ -0,0 +1,70 @@
+""" utility functions """
+
+import pandas as pd
+import numpy as np
+import torch
+
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy import sparse
+
+def fetch_dataset(n=None):
+    """ fetch data from disk and return a dataframe """
+
+    df_list = [pd.read_csv("winemag_dataset_%s.csv" % i) for i in range(6)]
+    full_df = pd.concat(df_list)
+
+    # give unique row names to all
+    full_df.index = range(full_df.shape[0])
+
+    if n is not None:
+        full_df = full_df.head(n)
+
+    print("Dataset fetched.")
+    return full_df
+
+def preprocess(full_df):
+    """ maybe clean up text, filter out rows with empty cells, remove stop words? """
+
+    clean_df = full_df
+
+    print("Dataset cleaned.")
+    return clean_df
+
+def conv_word_to_indexed_txt(txt_vec):
+    """ change txt into int indexes, return with dict of indexes & count of each word """
+
+    # transform words into integer indexes, comes out as n x m
+    # where n = # txt doc, m = # unique words for whole universe
+    vectorizer = CountVectorizer()
+    sparse_count_vec = vectorizer.fit_transform(txt_vec)
+
+    # create n x p list of words represented by ints,  where p = # words in each documentx
+    # written in such a convoluted way for speed optimization purposes 
+    x_vec, y_vec, count_vec = sparse.find(sparse_count_vec)
+
+    # add in duplicates
+    x_vec = np.repeat(x_vec, count_vec)
+    y_vec = np.repeat(y_vec, count_vec)
+
+    # sort the vecs
+    sort_ix = np.argsort(x_vec)
+    x_vec = x_vec[sort_ix]
+    y_vec = y_vec[sort_ix]
+
+    last_x = 0
+    indexed_txt_list = []
+    tmp_list = []
+    for x, y in zip(x_vec, y_vec):
+        if x > last_x:
+            indexed_txt_list.append(torch.FloatTensor(tmp_list))
+            tmp_list = [y]
+        else:
+            tmp_list.append(y)
+
+        last_x = x
+
+    # the dictionary key to match each int to the original word
+    vocab_dict = vectorizer.vocabulary_
+
+    print("Converted words to indexes of integers.")
+    return indexed_txt_list, vocab_dict
