49cf3b9d6e236a7431b195bc572a2ebd2b1aa0df
Sanjeev Tewani <sanjeev.k.tewani@gmail.com>
2019-11-13 18:48:21 -0500

    example presentation

--- notebooks/example.py
+++ notebooks/example.py
@@ -1,1 +1,12 @@
-from . import *
+from SommeliAI import util
+
+# %% [markdown]
+# # Motivation
+# We're interested in studying topic modelling -- specifically LDA and its variants -- in probabilistic programming languages
+# This is hard.
+
+# %%
+x = 1.
+print(x)
+
+# %%
