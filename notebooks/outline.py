f50cd703a857d489799b85c5aa5cb861e87de98b
Ray Li <raymond8908@gmail.com>
2019-11-17 22:25:18 -0500

    Made changed to outline.py

--- notebooks/outline.py
+++ notebooks/outline.py
@@ -24,5 +24,18 @@ from SommeliAI import util
 #(3) number of unique words in top 10 words for each
 
 # %%
-# This Section highlights a deeper dive into the data
-# - show 
+# Section 3: This Section highlights a deeper dive into the data
+# - show most frequent words
+# - show custom list of stop words to remove
+
+# %%
+# Secton 4: Show plain LDA run with custom stop words removed
+# - show the word cloud of top 10 words and reveal basically next 10 overlap words
+# - show word distribution again with spikes of next 10 words
+
+# %%
+# Section 5: Maybe data is noise? Not true as shown by CAVI
+# - show CAVI results
+
+# %%
+# Section 6: Maybe still have predictive power - sLDA
